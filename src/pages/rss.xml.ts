3b39de5c57a65dbd8bd1a18377e7fc431f939af2
Micheal Mac Donnacha <micheal.macdonnacha13@gmail.com>
2024-10-26 00:11:40 +0100

    test 2

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,7 +9,7 @@ export async function GET() {
   return rss({
     title: SITE.title,
     description: SITE.desc,
-    site: SITE.website + SITE.base,
+    site: SITE.website,
     items: sortedPosts.map(({ data, slug }) => ({
       link: `posts/${slug}/`,
       title: data.title,
